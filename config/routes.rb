a2019124a4035589b53765bfe7f9ab208d77d46a
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-01-13 19:25:44 +0900

    rails 課題２

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  
+  get "login" => "users#login_form"
+
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+
+  get "/" => "home#top"
+end
