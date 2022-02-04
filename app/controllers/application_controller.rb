2dbef06b4428c819f39a47ce51bc0ddd319e21d4
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-02-04 16:40:46 +0900

    プルリク希望

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
 
   def set_current_user
-    console
     @current_user = User.find_by(id: session[:user_id])
   end
 
